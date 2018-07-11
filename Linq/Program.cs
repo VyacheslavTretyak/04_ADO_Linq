9e975849f27bb50caf446d4831466130f39da582
VyacheslavTretyak <tretyak1c@gmail.com>
2018-07-11 17:43:47 +0300

    finished projects Linq and Linq2

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -10,6 +10,73 @@ namespace Linq
 	{
 		static void Main(string[] args)
 		{
+			List<Person> person = new List<Person>() {
+				new Person() { Name = "Andrey", Age = 24, City = "Kyiv" },
+				new Person() { Name = "Liza", Age = 18, City = "Moscow" },
+				new Person() { Name = "Oleg", Age = 15, City = "London" },
+				new Person() { Name = "Sergey", Age = 55, City = "Kyiv" },
+				new Person() { Name = "Sergey", Age = 32, City = "Kyiv" }
+			};
+			Console.WriteLine("\nPeoples Age < 25\n");
+			Console.WriteLine("Code for weakers : ");
+			IEnumerable <Person> res = from p in person
+							   where p.Age < 25
+							   select p;
+			foreach (var item in res)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			person.Where(p => p.Age < 25).ToList<Person>().ForEach(i => Console.WriteLine(i));
+
+			Console.WriteLine("\nPeoples that live in Kyiv\n");
+			Console.WriteLine("Code for weakers : ");
+			IEnumerable<Person> res1 = from p in person
+				  where p.City.ToUpper() == "KYIV"
+				  select p;
+			foreach (var item in res1)
+			{
+				Console.WriteLine(item.Name);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			person.Where(p => p.City.ToUpper() == "KYIV").ToList<Person>().ForEach(i => Console.WriteLine(i.Name));
+
+			Console.WriteLine("\nName of peoples that Live in Kyiv\n");
+			Console.WriteLine("Code for weakers : ");
+			IEnumerable<string> res2 = from p in person
+										where p.City.ToUpper() == "KYIV"
+										select p.Name;
+			foreach (var item in res2)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			person.Where(p => p.City.ToUpper() == "KYIV").Select(p=>p.Name).ToList<string>().ForEach(i => Console.WriteLine(i));
+
+
+			Console.WriteLine("\nPeoples that Live in Kyiv and Age > 35\n");
+			Console.WriteLine("Code for weakers : ");
+			IEnumerable<Person> res3 = from p in person
+									   where p.Age > 35 && p.City.ToUpper() == "KYIV"
+									   select p;
+			foreach (var item in res3)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			person.Where(p => p.City.ToUpper() == "KYIV" && p.Age > 35).ToList().ForEach(i => Console.WriteLine(i));
+
+			Console.WriteLine("\nPeoples that live in Moscow\n");
+			Console.WriteLine("Code for weakers : ");
+			IEnumerable<Person> res4 = from p in person
+									   where p.City.ToUpper() == "MOSCOW"
+									   select p;
+			foreach (var item in res4)
+			{
+				Console.WriteLine(item.Name);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			person.Where(p => p.City.ToUpper() == "MOSCOW").ToList<Person>().ForEach(i => Console.WriteLine(i.Name));
 		}
 	}
 }
---
+++ Linq2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2
+{
+	class Employee
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public int DepId { get; set;}
+		public override string ToString()
+		{
+			return $"Emploee {FirstName} {LastName}";
+		}
+	}
+	class Department
+	{
+		public int Id { get; set; }
+		public string Country { get; set; }
+		public string City { get; set; }
+		public override string ToString()
+		{
+			return $"Department {Country} {City}";
+		}
+	}
+	class Program
+	{
+		static void Main()
+		{
+			List<Department> departments = new List<Department>()
+			{
+				new Department() { Id = 1, Country = "Ukraine", City = "Donetsk" },
+				new Department() { Id = 2, Country = "Ukraine", City = "Kyiv" },
+				new Department() { Id = 3, Country = "France", City = "Paris" },
+				new Department() { Id = 4, Country = "Russia", City = "Moscow" }
+			};
+
+			List<Employee> employees = new List<Employee>()
+			{
+				new Employee() { Id = 1, FirstName = "Tamara", LastName = "Ivanova", Age = 22, DepId = 2 },
+				new Employee() { Id = 2, FirstName = "Nikita", LastName = "Larin", Age = 33, DepId = 1 },
+				new Employee() { Id = 3, FirstName = "Alica", LastName = "Ivanova", Age = 43, DepId = 3 },
+				new Employee() { Id = 4, FirstName = "Lida", LastName = "Marusyk", Age = 22, DepId = 2 },
+				new Employee() { Id = 5, FirstName = "Lida", LastName = "Voron", Age = 36, DepId = 4 },
+				new Employee() { Id = 6, FirstName = "Ivan", LastName = "Kalyta", Age = 22, DepId = 2 },
+				new Employee() { Id = 7, FirstName = "Nikita", LastName = " Krotov ", Age = 27, DepId = 4 }
+			};
+
+			Console.WriteLine("\n Выбрать имена и фамилии сотрудников, работающих в Украине, но не в Донецке\n");
+			Console.WriteLine("Code for weakers : ");
+			var res = from e in employees
+					  join d in departments on e.DepId equals d.Id
+						 where d.Country.ToUpper() == "UKRAINE" && d.City.ToUpper() != "DONETSK"
+						 select new { e.FirstName, e.LastName };
+			foreach (var item in res)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			employees.Join(departments, e => e.DepId, d => d.Id, (e, d) => new { e.FirstName, e.LastName, d.Country, d.City }).Where(e=>e.City.ToUpper() != "DONETSK" && e.Country.ToUpper()== "UKRAINE").ToList().ForEach(i => Console.WriteLine(i));
+
+			Console.WriteLine("\n Вывести список стран без повторений\n");
+			Console.WriteLine("Code for weakers : ");
+			var res1 = (from d in departments					  
+					  select d.Country).Distinct();
+			foreach (var item in res1)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			departments.Select(d=>d.Country).Distinct().ToList().ForEach(i => Console.WriteLine(i));
+			
+			
+			Console.WriteLine("\n Выбрать 3 - x первых сотрудников, возраст которых превышает 25 лет\n");
+			Console.WriteLine("Code for weakers : ");
+			var res2 = (from e in employees
+						where e.Age>25
+						select e).Take(3);
+			foreach (var item in res2)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			employees.Where(e=>e.Age>25).Take(3).ToList().ForEach(i => Console.WriteLine(i));
+
+
+			
+
+
+
+			Console.WriteLine("\n Выбрать имена, фамилии и возраст студентов из Киева, возраст которых меньше 23 года\n");
+			Console.WriteLine("Code for weakers : ");
+			var res3 = from e in employees
+					  join d in departments on e.DepId equals d.Id
+					  where e.Age<23 && d.City.ToUpper() == "KYIV"
+					  select new { e.FirstName, e.LastName, e.Age, d.City };
+			foreach (var item in res3)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("\nCode for hard programmers : ");
+			employees.Join(departments, e => e.DepId, d => d.Id, (e, d) => new { e.FirstName, e.LastName, e.Age, d.City }).Where(e => e.City.ToUpper() == "KYIV" && e.Age<23).ToList().ForEach(i => Console.WriteLine(i));
+		}
+	}
+}
